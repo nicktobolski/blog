f404f173b331338219548ed0f533a71b65b47c92
tobo <nicktobolski@protonmail.com>
2026-03-15 20:12:54 -0700

    Add back navigation link to home in MyApp component, enhancing user navigation experience on non-home pages.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const isHome = router.pathname === "/";
+
   return (
     <div className="container">
+      {!isHome && (
+        <nav className="back-nav">
+          <Link href="/">&larr; Home</Link>
+          <style jsx global>{`
+            .back-nav {
+              position: fixed;
+              top: 1.5rem;
+              left: 1.5rem;
+              z-index: 10;
+            }
+            .back-nav a {
+              color: #0070f3;
+              text-decoration: none;
+              font-size: 0.95rem;
+            }
+            .back-nav a:hover {
+              text-decoration: underline;
+            }
+          `}</style>
+        </nav>
+      )}
       <Component {...pageProps} />
     </div>
   );
